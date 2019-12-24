6435f680aca46c4cab04a8d5a79bb43630c664ff
abrk030 <abrk030@gmail.com>
2019-12-24 22:13:05 +0500

    images' size reduced

---
+++ main.js
@@ -0,0 +1,16 @@
+slideNumber=1;
+ShowSlide(slideNumber);
+
+function ShowSlide(n){
+ let slides = document.getElementsByClassName("main-4");
+ let dots = document.getElementsByClassName("dot");
+
+ for(i=0;i<slides.length;i++){
+   slides[i].style.display="none";
+ }
+ for(i=0;i<dots.length;i++){
+   dots[i].className="dot";
+ }
+ slides[n-1].style.display="block";
+ dots[n-1].className="dot active";
+}
